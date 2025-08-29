7fb8acf4f90e3b19b50385217234867254064449
keerthitharan <lakshmithara2011@gmail.com>
2025-08-29 01:07:37 -0500

    Fix Employer Portal and Routing Issues

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
-import { authAPI } from '../services/api';
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
